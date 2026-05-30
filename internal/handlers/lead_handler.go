83c46d293e97b6b013d9d81bce2cd6f54ed636c8
Bhavya Jain <bhavyajain4274@gmail.com>
2026-05-30 13:28:06 +0530

    handler layer created

---
+++ internal/handlers/lead_handler.go
@@ -0,0 +1,282 @@
+package handlers
+
+import (
+	"errors"
+	"net/http"
+
+	"github.com/BHAV0207/SuperLeap-BackendAssignment/internal/dto"
+	"github.com/BHAV0207/SuperLeap-BackendAssignment/internal/models"
+	"github.com/BHAV0207/SuperLeap-BackendAssignment/internal/services"
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
+type LeadHandler struct {
+	service *services.LeadService
+}
+
+func NewLeadHandler(service *services.LeadService) *LeadHandler {
+	return &LeadHandler{
+		service: service,
+	}
+}
+
+func (h *LeadHandler) CreateLead(c *gin.Context) {
+	var req dto.CreateLeadRequest
+
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, dto.ErrorResponse{
+			Success: false,
+			Error:   err.Error(),
+		})
+		return
+	}
+
+	lead, err := h.service.CreateLead(&req)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, dto.ErrorResponse{
+			Success: false,
+			Error:   err.Error(),
+		})
+		return
+	}
+
+	response := dto.LeadResponse{
+		ID:        lead.ID,
+		Name:      lead.Name,
+		Email:     lead.Email,
+		Phone:     lead.Phone,
+		Source:    lead.Source,
+		Status:    lead.Status,
+		CreatedAt: lead.CreatedAt,
+		UpdatedAt: lead.UpdatedAt,
+	}
+
+	c.JSON(http.StatusCreated, dto.SuccessResponse{
+		Success: true,
+		Data:    response,
+	})
+}
+
+func (h *LeadHandler) GetLeadByID(c *gin.Context) {
+	idParam := c.Param("id")
+
+	id, err := uuid.Parse(idParam)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, dto.ErrorResponse{
+			Success: false,
+			Error:   "invalid UUID format",
+		})
+		return
+	}
+
+	lead, err := h.service.GetLeadByID(id)
+	if errors.Is(err, gorm.ErrRecordNotFound) {
+		c.JSON(http.StatusNotFound, dto.ErrorResponse{
+			Success: false,
+			Error:   "lead not found",
+		})
+		return
+	}
+
+	response := dto.LeadResponse{
+		ID:        lead.ID,
+		Name:      lead.Name,
+		Email:     lead.Email,
+		Phone:     lead.Phone,
+		Source:    lead.Source,
+		Status:    lead.Status,
+		CreatedAt: lead.CreatedAt,
+		UpdatedAt: lead.UpdatedAt,
+	}
+
+	c.JSON(http.StatusOK, dto.SuccessResponse{
+		Success: true,
+		Data:    response,
+	})
+}
+
+func (h *LeadHandler) GetAllLeads(c *gin.Context) {
+	statusQuery := c.Query("status")
+
+	var status *models.LeadStatus
+
+	if statusQuery != "" {
+		s := models.LeadStatus(statusQuery)
+		status = &s
+	}
+
+	leads, err := h.service.GetAllLeads(status)
+	if err != nil {
+
+		c.JSON(http.StatusInternalServerError, dto.ErrorResponse{
+			Success: false,
+			Error:   err.Error(),
+		})
+
+		return
+	}
+
+	var response []dto.LeadResponse
+
+	for _, lead := range leads {
+		response = append(response, dto.LeadResponse{
+			ID:        lead.ID,
+			Name:      lead.Name,
+			Email:     lead.Email,
+			Phone:     lead.Phone,
+			Source:    lead.Source,
+			Status:    lead.Status,
+			CreatedAt: lead.CreatedAt,
+			UpdatedAt: lead.UpdatedAt,
+		})
+	}
+
+	c.JSON(http.StatusOK, dto.SuccessResponse{
+		Success: true,
+		Data:    response,
+	})
+}
+
+func (h *LeadHandler) UpdateLead(c *gin.Context) {
+	idParam := c.Param("id")
+
+	id, err := uuid.Parse(idParam)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, dto.ErrorResponse{
+			Success: false,
+			Error:   "invalid UUID format",
+		})
+		return
+	}
+
+	var req dto.UpdateLeadRequest
+
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, dto.ErrorResponse{
+			Success: false,
+			Error:   err.Error(),
+		})
+		return
+	}
+
+	lead, err := h.service.UpdateLead(id, req)
+	if errors.Is(err, gorm.ErrRecordNotFound) {
+		c.JSON(http.StatusNotFound, dto.ErrorResponse{
+			Success: false,
+			Error:   "lead not found",
+		})
+		return
+	} else if err != nil {
+		c.JSON(http.StatusInternalServerError, dto.ErrorResponse{
+			Success: false,
+			Error:   err.Error(),
+		})
+		return
+	}
+
+	response := dto.LeadResponse{
+		ID:        lead.ID,
+		Name:      lead.Name,
+		Email:     lead.Email,
+		Phone:     lead.Phone,
+		Source:    lead.Source,
+		Status:    lead.Status,
+		CreatedAt: lead.CreatedAt,
+		UpdatedAt: lead.UpdatedAt,
+	}
+
+	c.JSON(http.StatusOK, dto.SuccessResponse{
+		Success: true,
+		Data:    response,
+	})
+	
+}
+
+func (h *LeadHandler) UpdateLeadStatus(c *gin.Context) {
+	idParam := c.Param("id")
+
+	id, err := uuid.Parse(idParam)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, dto.ErrorResponse{
+			Success: false,
+			Error:   "invalid UUID format",
+		})
+		return
+	}
+
+	var req dto.UpdateLeadStatusRequest
+
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, dto.ErrorResponse{
+			Success: false,
+			Error:   err.Error(),
+		})
+		return
+	}
+
+	lead, err := h.service.UpdateLeadStatus(id, req.Status)
+	if errors.Is(err, gorm.ErrRecordNotFound) {
+		c.JSON(http.StatusNotFound, dto.ErrorResponse{
+			Success: false,
+			Error:   "lead not found",
+		})
+		return
+	} else if err != nil {
+		c.JSON(http.StatusInternalServerError, dto.ErrorResponse{
+			Success: false,
+			Error:   err.Error(),
+		})
+		return
+	}
+
+	response := dto.LeadResponse{
+		ID:        lead.ID,
+		Name:      lead.Name,
+		Email:     lead.Email,
+		Phone:     lead.Phone,
+		Source:    lead.Source,
+		Status:    lead.Status,
+		CreatedAt: lead.CreatedAt,
+		UpdatedAt: lead.UpdatedAt,
+	}
+
+	c.JSON(http.StatusOK, dto.SuccessResponse{
+		Success: true,
+		Data:    response,
+	})
+}
+
+func (h *LeadHandler) DeleteLead(c *gin.Context) {
+	idParam := c.Param("id")
+
+	id, err := uuid.Parse(idParam)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, dto.ErrorResponse{
+			Success: false,
+			Error:   "invalid UUID format",
+		})
+		return
+	}
+
+	err = h.service.DeleteLead(id)
+	if errors.Is(err, gorm.ErrRecordNotFound) {
+		c.JSON(http.StatusNotFound, dto.ErrorResponse{
+			Success: false,
+			Error:   "lead not found",
+		})
+		return
+	} else if err != nil {
+		c.JSON(http.StatusInternalServerError, dto.ErrorResponse{
+			Success: false,
+			Error:   err.Error(),
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, dto.SuccessResponse{
+		Success: true,
+		Data:    "lead deleted successfully",
+	})
+}	
--- internal/services/lead_status.go
+++ internal/services/lead_status.go
@@ -1,4 +1,4 @@
-package service
+package services
 
 import "github.com/BHAV0207/SuperLeap-BackendAssignment/internal/models"
 
